e6885c8bde71f5d0c0da5ae2a189060518bcb1ea
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-06-24 07:41:40 +0100

    WiP

--- helper.php
+++ helper.php
@@ -95,4 +95,76 @@ class TplNPEU6Helper
 
 		return self::$template;
     }
+    
+    /**
+     * Gets the current template.
+     *
+     * @return object
+     */
+    public static function stripPunctuation($text)
+    {
+        if (!is_string($text)) {
+            trigger_error('Function \'strip_punctuation\' expects argument 1 to be an string', E_USER_ERROR);
+            return false;
+        }
+        $text = html_entity_decode($text, ENT_QUOTES);
+
+        $urlbrackets = '\[\]\(\)';
+        $urlspacebefore = ':;\'_\*%@&?!' . $urlbrackets;
+        $urlspaceafter = '\.,:;\'\-_\*@&\/\\\\\?!#' . $urlbrackets;
+        $urlall = '\.,:;\'\-_\*%@&\/\\\\\?!#' . $urlbrackets;
+
+        $specialquotes = '\'"\*<>';
+
+        $fullstop = '\x{002E}\x{FE52}\x{FF0E}';
+        $comma = '\x{002C}\x{FE50}\x{FF0C}';
+        $arabsep = '\x{066B}\x{066C}';
+        $numseparators = $fullstop . $comma . $arabsep;
+
+        $numbersign = '\x{0023}\x{FE5F}\x{FF03}';
+        $percent = '\x{066A}\x{0025}\x{066A}\x{FE6A}\x{FF05}\x{2030}\x{2031}';
+        $prime = '\x{2032}\x{2033}\x{2034}\x{2057}';
+        $nummodifiers = $numbersign . $percent . $prime;
+        $return = preg_replace(
+        array(
+            // Remove separator, control, formatting, surrogate,
+            // open/close quotes.
+            '/[\p{Z}\p{Cc}\p{Cf}\p{Cs}\p{Pi}\p{Pf}]/u',
+            // Remove other punctuation except special cases
+            '/\p{Po}(?<![' . $specialquotes .
+            $numseparators . $urlall . $nummodifiers . '])/u',
+            // Remove non-URL open/close brackets, except URL brackets.
+            '/[\p{Ps}\p{Pe}](?<![' . $urlbrackets . '])/u',
+            // Remove special quotes, dashes, connectors, number
+            // separators, and URL characters followed by a space
+            '/[' . $specialquotes . $numseparators . $urlspaceafter .
+            '\p{Pd}\p{Pc}]+((?= )|$)/u',
+            // Remove special quotes, connectors, and URL characters
+            // preceded by a space
+            '/((?<= )|^)[' . $specialquotes . $urlspacebefore . '\p{Pc}]+/u',
+            // Remove dashes preceded by a space, but not followed by a number
+            '/((?<= )|^)\p{Pd}+(?![\p{N}\p{Sc}])/u',
+            // Remove consecutive spaces
+            '/ +/',
+            ), ' ', $text);
+        $return = str_replace('/', '_', $return);
+        return str_replace("'", '', $return);
+    }
+    
+    /**
+     * Creates an HTML-friendly string for use in id's
+     *
+     * @param string $text
+     * @return string
+     * @access public
+     */
+    public static function htmlID($text)
+    {
+        if (!is_string($text)) {
+            trigger_error('Function \'html_id\' expects argument 1 to be an string', E_USER_ERROR);
+            return false;
+        }
+        $return = strtolower(trim(preg_replace('/\s+/', '-', self::stripPunctuation($text))));
+        return $return;
+    }
 }
--- index.php
+++ index.php
@@ -20,10 +20,17 @@ use \Michelf\Markdown;
 JLoader::register('TplNPEU6Helper', __DIR__ . '/helper.php');
 
 
-$app    = JFactory::getApplication();
-$doc    = JFactory::getDocument();
+$app   = JFactory::getApplication();
+$doc   = JFactory::getDocument();
+$input = JFactory::getApplication()->input;
+$db    = JFactory::getDBO();
+$user  = JFactory::getUser();
 
+#echo '<pre>'; var_dump($app); echo '</pre>'; exit;
 #echo '<pre>'; var_dump($doc); echo '</pre>'; exit;
+#echo '<pre>'; var_dump($input); echo '</pre>'; exit;
+echo '<pre>'; var_dump($user); echo '</pre>'; exit;
+
 
 // Set variables otherwise declared in error.php:
 if (!isset($is_error)) {
@@ -36,13 +43,46 @@ if (!isset($menu_item)) {
     $menu_item = TplNPEU6Helper::get_menu_item();
 }
 $menu_root = explode('/', $menu_item->route)[0];
-#echo '<pre>'; var_dump($menu_item); echo '</pre>'; exit;
+echo '<pre>'; var_dump($menu_item); echo '</pre>'; exit;
 #echo '<pre>'; var_dump($menu_item->params->get('hero_image')); echo '</pre>'; exit;
 #echo '<pre>'; var_dump($menu_root); echo '</pre>'; exit;
+#echo '<pre>'; var_dump($menu_item->access); echo '</pre>'; exit;
 
 // Brand
 $page_brand      = TplNPEU6Helper::get_brand();
 #echo '<pre>'; var_dump($page_brand); echo '</pre>'; exit;
+
+
+// Modules sometimes need to add scripts and styles ot the $doc but if we wait to use <jdoc> they're
+// processed too late and get missed.
+// Pre-rendering all modules here to avoid that. Not sure if there will be other consequences.
+jimport('joomla.application.module.helper');
+$modules__header_nav_bar  = JHtml::_('content.prepare', '{loadposition 2-header-nav-bar,basic}');
+$modules__main_breadcumbs = JHtml::_('content.prepare', '{loadposition 3-main-breadcrumbs,basic}');
+$modules__main_upper      = JHtml::_('content.prepare', '{loadposition 3-main-upper,basic}');
+$modules__sidebar_top     = JHtml::_('content.prepare', '{loadposition 4-sidebar-top,sidebar}');
+$modules__sidebar_bottom  = JHtml::_('content.prepare', '{loadposition 4-sidebar-bottom,sidebar}');
+$modules__main_lower      = JHtml::_('content.prepare', '{loadposition 3-main-lower,basic}');
+$modules__bottom          = JHtml::_('content.prepare', '{loadposition 5-bottom,block}');
+$modules__footer_top      = JHtml::_('content.prepare', '{loadposition 6-footer-top,block}');
+
+
+// Branded search pages need an extra query string parameter to limit the search to just that part
+// of the site. This may not be the best place to establish this, so keep that in mind.
+$page_search_area = '';
+if ($page_brand->alias != 'npeu') {
+    
+        $query = '
+            SELECT id
+            FROM #__finder_taxonomy
+            WHERE title = "' . $page_brand->name . '";
+        ';
+        $db->setQuery($query);
+        $page_search_area = '&t[]=' . $db->loadResult();
+}
+
+
+
 
 // Level 1 menu items that are not mainmenu or are landing pages:
 // (Note this isn't robust as if new non-brand menus are created or names change, this will fail)
@@ -68,8 +108,13 @@ $page_template_params = $page_template->params->toObject();
 $page_heading = isset($doc->article)
               ? $doc->article->title
               : $menu_item->title;
+              
+#echo '<pre>'; var_dump($page_heading); echo '</pre>'; exit;
+$page_title   = $page_heading;
 
-$page_title   = $page_heading . ' | ' . $page_template_params->site_title;
+if ($page_heading != $page_template_params->site_title) {
+   $page_title .= ' | ' . $page_template_params->site_title;
+}
 
 $page_has_article              = !empty($doc->article);
 $page_has_priority_content     = !empty($doc->article->fulltext);
@@ -77,7 +122,80 @@ $page_has_sidebar_top          = $doc->countModules('4-sidebar-top');
 $page_has_sidebar_section_menu = $doc->countModules('4-sidebar-section-menu');
 $page_has_sidebar_bottom       = $doc->countModules('4-sidebar-bottom');
 
+
+// Sort out ToC. May make this a module to make it clearer to others where this comes from:
+$page_toc = '';
+if ($page_has_article) {
+    $min_h2_count = 3;
+    
+    // ToC requires id's on headers, so add them if not already present.
+    // Note this is a back-up, ideally the editor will create them so they're saved into the article.
+    #$h2s = preg_match_all('#<h2[^>]*(id="([^"]+)")?[^>]*>#', $doc->article->text, $matches, PREG_SET_ORDER);
+    preg_match_all('#<h2[^>]*>([^<]+)</h2>#', $doc->article->text, $matches, PREG_SET_ORDER);
+    
+    if (count($matches) >= $min_h2_count) {
+        
+        $page_toc .= '<div class="c-panel  c-panel--very-light  t-neutral  u-space--above  u-space--above">' . "\n";
+        $page_toc .= '<nav class="c-panel--module" aria-label="table of contents">' . "\n";
+        $page_toc .= '<div class="n-section-menu">' . "\n";
+        $page_toc .= '<h2>On this page</h2>' . "\n";
+        $page_toc .= '<ul class="n-section-menu__list">' . "\n";
+        
+        foreach ($matches as $match) {
+            preg_match('#id="[^"]+"#', $match[0], $id_match);
+            #echo '<pre>'; var_dump($id_match); echo '</pre>'; #exit;
+            if(!isset($id_match[0])) {
+                $h2_id = TplNPEU6Helper::htmlID($match[1]);
+                $new_h2 = str_replace('<h2', '<h2 id="' . $h2_id . '"', $match[0]);
+                
+                $doc->article->text      = str_replace($match[0], $new_h2, $doc->article->text);
+                $doc->article->fulltext  = str_replace($match[0], $new_h2, $doc->article->fulltext);
+                $doc->article->introtext = str_replace($match[0], $new_h2, $doc->article->introtext);
+            } else {
+                $h2_id = $id_match[0];
+            }
+            $page_toc .= '<li class="n-section-menu__item"><a href="#' . $h2_id . '" class="n-section-menu__link">' . $match[1] . '</a></li>' . "\n";
+        }
+        $page_toc .= '</ul>' . "\n";
+        $page_toc .= '</div>' . "\n";
+        $page_toc .= '</nav>' . "\n";
+        $page_toc .= '</div>' . "\n";
+        
+        $page_has_sidebar_bottom++;
+    }
+}
+
+
+#echo '<pre>'; var_dump($page_has_article); echo '</pre>'; exit;
+#echo '<pre>'; var_dump($page_has_priority_content); echo '</pre>'; #exit;
+#echo '<pre>'; var_dump($page_has_sidebar_top); echo '</pre>'; #exit;
+#echo '<pre>'; var_dump($page_has_sidebar_section_menu); echo '</pre>'; #exit;
+#echo '<pre>'; var_dump($page_has_sidebar_bottom); echo '</pre>'; #exit;
+
 $page_has_pull_outs = $page_has_priority_content || $page_has_sidebar_top || $page_has_sidebar_section_menu || $page_has_sidebar_bottom;
+#echo '<pre>'; var_dump($page_has_pull_outs); echo '</pre>'; #exit;
+
+// Handle page-specific disabling of sidebar modules:
+$page_default_view = $menu_item->query['view'];
+#echo '<pre>'; var_dump($page_default_view); echo '</pre>'; exit;
+$page_current_view = $input->get('view');
+#echo '<pre>'; var_dump($page_current_view); echo '</pre>'; exit;
+$page_is_component_root = $page_default_view == $page_current_view;
+
+$page_disable_modules = $menu_item->params->get('disable_modules');
+
+// We can't FORCE show modules, because there may not be any, but we can force DON'T SHOW modules:
+if (
+    $page_disable_modules == 'always'
+ || $page_disable_modules == 'root' && $page_is_component_root
+ || $page_disable_modules == 'sub' && !$page_is_component_root
+) {
+    $page_has_pull_outs = false;
+}
+
+#echo '<pre>'; var_dump($page_has_pull_outs); echo '</pre>'; exit;
+
+$menu_item->params->get('hero_image');
 
 $page_has_main_lower = $doc->countModules('3-main-lower');
 
@@ -112,7 +230,7 @@ $page_script      = !empty($page_head_data['script']) ? $page_head_data['script'
 
 #echo '<pre>'; var_dump($page_stylesheets); echo '</pre>'; #exit;
 #echo '<pre>'; var_dump($page_styles); echo '</pre>'; #exit;
-#echo '<pre>'; var_dump($page_scripts); echo '</pre>'; #exit;
+#echo '<pre>'; var_dump($page_scripts); echo '</pre>'; exit;
 #echo '<pre>'; var_dump($page_script); echo '</pre>'; exit;
 
 
@@ -128,14 +246,14 @@ $page_has_navbar   = $page_template_params->show_navbar && $doc->countModules('2
 
 // Hero image / Carousel:
 $page_hero         = (array) $menu_item->params->get('hero_image');
+#echo '<pre>'; var_dump($page_hero); echo '</pre>'; exit;
 
-$page_has_hero     = !empty($page_hero);
+$page_has_hero     = !empty($page_hero['hero_image0']->image);
 $page_has_carousel = $page_has_hero && count($page_hero) > 1;
 
 $page_hero         = ($page_has_hero && !$page_has_carousel) ? $page_hero['hero_image0'] : false;
 $page_carousel     = ($page_has_hero && $page_has_carousel)  ? $page_hero : false;
 
-#echo '<pre>'; var_dump($page_hero); echo '</pre>'; exit;
 
 
 // Meta (?):
