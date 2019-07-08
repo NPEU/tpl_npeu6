7d0b3aacc6d8b69f5586fb2ce656c08571f1d358
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-07-08 07:50:33 +0100

    Fixed CTA URL typo

--- html/mod_text/default.php
+++ html/mod_text/default.php
@@ -18,9 +18,9 @@ if ($hx == 'h1') {
 <div class="c-panel  u-fill-height  u-padding--sides--l">
     <div class="c-longform-content">
         <?php echo $module->content; ?>
-        <?php if (!empty($params->get('cta_text')) && !empty($params->get('cta_text'))): ?>
+        <?php if (!empty($params->get('cta_url')) && !empty($params->get('cta_text'))): ?>
         <p>
-            <a href="<?php echo $params->get('cta_text'); ?>" class="cta  cta--has-icon"><?php echo $params->get('cta_text'); ?><svg display="none" class="icon" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg></a>
+            <a href="<?php echo $params->get('cta_url'); ?>" class="cta  cta--has-icon"><?php echo $params->get('cta_text'); ?><svg display="none" class="icon" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg></a>
         </p>
         <?php endif; ?>
     </div>
