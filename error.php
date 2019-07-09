5fbcd7393eadcc668f8916b9c112542e131c720a
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-07-09 21:23:24 +0100

    Added 2 cols to error content and tidied

--- error.php
+++ error.php
@@ -13,7 +13,7 @@ $error_code = $this->error->getCode();
 ob_start();
 ?>
     <?php if ($error_code == 404) : ?>
-    <div id="hero" class="c-hero  c-hero--reversed  d-bands--top  d-bands--bottom  u-space--below">
+    <div id="hero" class="c-hero  c-hero--reversed  d-bands--top  d-bands--bottom">
         <div class="c-hero__image">
             <div class="l-proportional-container  l-proportional-container--3-1  l-proportional-container--4-1--wide  hero-image">
                 <div class="l-proportional-container__content">
@@ -46,25 +46,37 @@ ob_start();
                     </div>
 
                     <div class="c-panel">
-                        <p>You could try searching:</p>
+                    
+                        <div class="l-col-to-row--flush-edge-gutters">
+                            <div class="l-col-to-row  l-col-to-row--gutter--large">
+                                <div class="l-col-to-row__item  ff-width-100--40--50">
+                                    <p>You could try searching:</p>
 
-                        <form action="/search" id="searchform" class="" method="GET">
-                            <span class="composite">
-                                <input type="search" class="search-form__field" id="search" placeholder="Search" name="q" value="" aria-label="Search">
-                                <button class="search-form__submit" type="submit">
-                                    <span>Search</span>
-                                </button>
-                            </span>
-                        </form>
+                                    <form action="/search" id="searchform" class="" method="GET">
+                                        <span class="composite">
+                                            <input type="search" class="search-form__field" id="search" placeholder="Search" name="q" value="" aria-label="Search">
+                                            <button class="search-form__submit" type="submit">
+                                                <span>Search</span>
+                                            </button>
+                                        </span>
+                                    </form>
+                        
+                                </div>
+                                <div class="l-col-to-row__item  ff-width-100--40--50">
 
-                        <p>Or one of these links:</p>
-                        <ul>
-                            <li><a href="https://www.npeu.ox.ac.uk"><span>Home</span></a></li>
-                            <li><a href="https://www.npeu.ox.ac.uk/ctu/trials"><span>CTU Trials Portfolio</span></a></li>
-                            <li><a href="https://www.npeu.ox.ac.uk/sheer"><span>SHEER Portfolio</span></a></li>
-                            <li><a href="https://www.npeu.ox.ac.uk/prumnhc"><span>PRU-MNHC</span></a></li>
-                            <li><a href="https://www.npeu.ox.ac.uk/about"><span>About the NPEU</span></a></li>
-                        </ul>
+                                    <p>Or one of these links:</p>
+                                    <ul>
+                                        <li><a href="https://www.npeu.ox.ac.uk"><span>Home</span></a></li>
+                                        <li><a href="https://www.npeu.ox.ac.uk/ctu/trials"><span>CTU Trials Portfolio</span></a></li>
+                                        <li><a href="https://www.npeu.ox.ac.uk/sheer"><span>SHEER Portfolio</span></a></li>
+                                        <li><a href="https://www.npeu.ox.ac.uk/prumnhc"><span>PRU-MNHC</span></a></li>
+                                        <li><a href="https://www.npeu.ox.ac.uk/about"><span>About the NPEU</span></a></li>
+                                    </ul>
+                                    
+                                </div>                              
+                            </div>
+                        </div>
+                        
                     </div>
                     
                 </div>
@@ -79,7 +91,7 @@ ob_start();
         </main>
     </div>
     <?php endif; ?>
-    <footer class="sticky-footer  t-npeu  d-bands--top" role="contentinfo" id="page-footer">
+    <footer class="sticky-footer" role="contentinfo" id="page-footer">
 
         <div class="l-distribute-wrap">
             <div class="l-distribute  l-distribute--gutter--small  l-distribute--limit-15">
