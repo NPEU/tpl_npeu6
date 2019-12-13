1f8581a16db0517df504a595153f8c24653df30a
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-12-13 11:41:49 +0000

    Added mod_custom override

---
+++ html/mod_custom/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   Copyright (C) NPEU 2019.
+ * @license     MIT License; see LICENSE.md
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<div class="c-user-content<?php echo $moduleclass_sfx; ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage'); ?>)"<?php endif; ?> >
+	<?php echo $module->content; ?>
+</div>
+
