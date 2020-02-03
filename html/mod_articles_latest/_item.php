dcd691381bc1fd261a1e213bea249b77d3fb8c04
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-02-03 13:13:06 +0000

    Full link toggle.

--- html/mod_articles_latest/_item.php
+++ html/mod_articles_latest/_item.php
@@ -16,12 +16,12 @@ $i = isset($i) ? $i : 0;
 $card_data = array();
 
 $card_data['theme']        = $theme;
+$card_data['full_link']    = $i == 1 ? false : true;
 $card_data['link']         = $item->link;
 $card_data['image']        = !empty($item->skip_image) ? false : $fields[0]->rawvalue;
 $card_data['image_alt']    = $fields[1]->rawvalue;
 $card_data['title']        = $item->title;
 $card_data['body']         = $i == 1 ? $item->introtext : '';
-$card_data['cta']          = $i == 1 ? $item->introtext : '';
 $card_data['publish_date'] = $item->publish_up;
 $card_data['date_format']  = $date_format;
 
