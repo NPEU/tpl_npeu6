49efc56bf736599207d607e2c38545f9267798be
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-09-14 16:05:51 +0100

    Remove redundant wrap and item classes.

--- html/mod_funder/default.php
+++ html/mod_funder/default.php
@@ -34,31 +34,29 @@ $logo_svg_doc = $logo_image->getDocument();
 $logo_title = $logo_svg_doc->getElementsByTagName('title')[0]->getValue();
 ?>
 <div class="d-bands--bottom  <?php echo $theme; ?>">
-    <div class="l-col-to-row-wrap">
-        <div class="l-col-to-row">            
-            <?php if (!empty($params->get('image'))): ?>
-            <div class="l-col-to-row__item  ff-width-100--40--50">
-                <div class="u-image-cover  u-image-cover--min-20  js-image-cover">
-                    <div class="u-image-cover__inner">
-                        <img class="u-image-cover__image" src="<?php echo $params->get('image'); ?>" width="150" alt="">
-                    </div>
+    <div class="l-col-to-row">            
+        <?php if (!empty($params->get('image'))): ?>
+        <div class="ff-width-100--40--50">
+            <div class="u-image-cover  u-image-cover--min-20  js-image-cover">
+                <div class="u-image-cover__inner">
+                    <img class="u-image-cover__image" src="<?php echo $params->get('image'); ?>" width="150" alt="">
                 </div>
             </div>
-            <?php endif; ?>
-            
-            <div class="l-col-to-row__item  ff-width-100--40--50">
-                <div class="c-panel  u-fill-heightX  u-max-measure">
-                    
-                    <div class="u-text-align--center u-fill-width u-space--below">
-                        <div>
-                            <a href="<?php echo $params->get('brand_url'); ?>" class="c-badge  c-badge--limit-height  l-center" rel="external noopener noreferrer" target="_blank">
-                                <img src="/assets/images/brand-logos/funder/<?php echo $brand->alias; ?>-logo.svg" onerror="this.src='/assets/images/brand-logos/affiliate/<?php echo $brand->alias; ?>-logo.png'; this.onerror=null;" alt="Logo: <?php echo $logo_title; ?>" height="80">
-                            </a>
-                        </div>
+        </div>
+        <?php endif; ?>
+        
+        <div class="ff-width-100--40--50">
+            <div class="c-panel  u-fill-heightX  u-max-measure">
+                
+                <div class="u-text-align--center u-fill-width u-space--below">
+                    <div>
+                        <a href="<?php echo $params->get('brand_url'); ?>" class="c-badge  c-badge--limit-height  l-center" rel="external noopener noreferrer" target="_blank">
+                            <img src="/assets/images/brand-logos/funder/<?php echo $brand->alias; ?>-logo.svg" onerror="this.src='/assets/images/brand-logos/affiliate/<?php echo $brand->alias; ?>-logo.png'; this.onerror=null;" alt="Logo: <?php echo $logo_title; ?>" height="80">
+                        </a>
                     </div>
-                    <div class="c-user-content">
-                        <?php echo $params->get('statement'); ?>
-                    </div>
+                </div>
+                <div class="c-user-content">
+                    <?php echo $params->get('statement'); ?>
                 </div>
             </div>
         </div>
