7ecab0c25bf2c1ae161bb3622e3c97b44e02046b
Andy Kirk <andy@andykirk.net>
2021-01-25 11:09:29 +0000

    Reorganise form to make more inline fo NoCSS.

--- html/mod_login/default_logout.php
+++ html/mod_login/default_logout.php
@@ -13,15 +13,15 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 ?>
 <?php if(!$user->get('guest')): ?>
-<a href="/user-profile"><span><strong><?php echo $user->username; ?></strong></span></a>
-(
 <form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure', 0)); ?>" method="post" id="login-form" class="c-single-link-form">
+    <a href="/user-profile"><span><strong><?php echo $user->username; ?></strong></span></a>
+    (
     <button name="Submit"><span><?php echo JText::_('JLOGOUT'); ?></span></button>
     <input type="hidden" name="option" value="com_users" />
     <input type="hidden" name="task" value="user.logout" />
     <input type="hidden" name="return" value="<?php echo $return; ?>" />
     <?php echo JHtml::_('form.token'); ?>
+    )
 </form>
-)
 <?php if($user->get('is_staff')): ?> | <a href="/administrator"><span>Admin</span></a><?php endif; ?>
 <?php endif; ?>
