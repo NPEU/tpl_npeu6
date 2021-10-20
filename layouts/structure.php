6999e628ac22b762aa16f7a34cf954b3a3ec9152
Andy Kirk <andy@andykirk.net>
2021-10-20 16:14:13 +0100

    Update favicons.

--- layouts/structure.php
+++ layouts/structure.php
@@ -166,7 +166,7 @@
     <?php endforeach; ?>
     </style>
     <?php endif; ?>
-
+    
     <link rel="apple-touch-icon" sizes="180x180" href="/templates/npeu6/favicon/<?php echo $page_brand_folder; ?>apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/templates/npeu6/favicon/<?php echo $page_brand_folder; ?>favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/templates/npeu6/favicon/<?php echo $page_brand_folder; ?>favicon-16x16.png">
