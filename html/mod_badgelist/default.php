12a363d8fa995a18b9719109890bb68d9ded2398
Andy Kirk <andy@andykirk.net>
2021-12-06 16:57:30 +0000

    Add badgelist.

---
+++ html/mod_badgelist/default.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_badgelist
+ *
+ * @copyright   Copyright (C) NPEU 2021.
+ * @license     MIT License; see LICENSE.md
+ */
+
+defined('_JEXEC') or die;
+
+
+$doc = JFactory::getDocument();
+
+$layout_classes = ['l-layout', 'l-row'];
+if (!empty($params->get('list_layout'))) {
+    $layout_classes[] = 'l-' . $params->get('list_layout');
+}
+
+if (!empty($params->get('list_gutter'))) {
+    $layout_classes[] = 'l-gutter--' . $params->get('list_gutter');
+}
+
+if (!empty($params->get('list_basis'))) {
+    $layout_classes[] = 'l-basis-' . $params->get('list_basis');
+}
+
+?>
+<div class="<?php echo implode("  ", $layout_classes); ?>">
+    <div class="l-layout__inner">
+    <?php foreach ($badges as $badge) : ?>
+        <div class="l-box">
+            <a href="<?php echo $badge->params->logo_url; ?>" class="c-badge c-badge--limit-height--<?php echo $badge->params->limit_height; ?>" rel="external noopener noreferrer" target="_blank">
+                <img alt="Logo: <?php echo $badge->alt; ?>" height="<?php echo $badge->params->limit_height; ?>0" onerror="this.src='<?php echo $badge->logo_png_path; ?>'; this.onerror=null;" src="<?php echo $badge->logo_svg_path; ?>">
+            </a>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
