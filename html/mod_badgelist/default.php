3d414d52234d9481cfbd0ad6efda98faa0bcd52f
Andy Kirk <andy@andykirk.net>
2021-12-14 16:22:58 +0000

    Update CSS

--- html/mod_badgelist/default.php
+++ html/mod_badgelist/default.php
@@ -21,6 +21,10 @@ if (!empty($params->get('list_gutter'))) {
     $layout_classes[] = 'l-gutter--' . $params->get('list_gutter');
 }
 
+if (!empty($params->get('flush_gutter'))) {
+    $layout_classes[] = 'l-flush-edge-gutter';
+}
+
 if (!empty($params->get('list_basis'))) {
     $layout_classes[] = 'l-basis-' . $params->get('list_basis');
 }
@@ -29,11 +33,11 @@ if (!empty($params->get('list_basis'))) {
 <div class="<?php echo implode("  ", $layout_classes); ?>">
     <div class="l-layout__inner">
     <?php foreach ($badges as $badge) : ?>
-        <div class="l-box">
+        <p class="l-box">
             <a href="<?php echo $badge->params->logo_url; ?>" class="c-badge c-badge--limit-height--<?php echo $badge->params->limit_height; ?>" rel="external noopener noreferrer" target="_blank">
                 <img alt="Logo: <?php echo $badge->alt; ?>" height="<?php echo $badge->params->limit_height; ?>0" onerror="this.src='<?php echo $badge->logo_png_path; ?>'; this.onerror=null;" src="<?php echo $badge->logo_svg_path; ?>">
             </a>
-        </div>
+        </p>
         <?php endforeach; ?>
     </div>
 </div>
