045ef5cbbec3fee1e36d1efc939e8ebd901f8a91
Andy Kirk <andy@andykirk.net>
2022-01-12 11:13:37 +0000

    Improve message and close button fallbacks.

--- html/layouts/joomla/system/message.php
+++ html/layouts/joomla/system/message.php
@@ -17,13 +17,15 @@ $msgList = $displayData['msgList'];
 <div id="system-messages">
     <?php foreach ($msgList as $type => $msgs) : ?>
     <?php $dismiss = ($type == 'message' || $type == 'notice') ? ' data-js="close-button"' : ''; ?>
-    <div class="c-system-message  t-<?php echo $type == 'message' ? 'success' : $type; ?>"<?php echo $dismiss; ?>>
-        <?php // This requires JS so we should add it trough JS. Progressive enhancement and stuff. ?>
-        <?php /*<a class="close" data-dismiss="alert">×</a>*/?>
-        <p><strong><?php echo JText::_($type); ?></strong></p>
-        <?php foreach ($msgs as $msg) : ?>
-        <p><?php echo $msg; ?></p>
-        <?php endforeach; ?>        
+    <div class="c-system-message  t-<?php echo $type == 'message' ? 'success' : $type; ?>">
+        <fieldset role="presentation"<?php echo $dismiss; ?>>
+            <?php // This requires JS so we should add it trough JS. Progressive enhancement and stuff. ?>
+            <?php /*<a class="close" data-dismiss="alert">×</a>*/?>
+            <p><strong><?php echo JText::_($type); ?></strong></p>
+            <?php foreach ($msgs as $msg) : ?>
+            <p><?php echo $msg; ?></p>
+            <?php endforeach; ?>        
+        </fieldset>
     </div>
 
     <?php endforeach; ?>
