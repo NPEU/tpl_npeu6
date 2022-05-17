cd0dbe3bc11052a7bf77569f6a9493227baef41b
Andy Kirk <andy@andykirk.net>
2022-05-17 11:49:30 +0100

    Add progenitor image.

--- html/mod_articles_latest/_item.php
+++ html/mod_articles_latest/_item.php
@@ -14,7 +14,69 @@ if (!empty($_SERVER['JTV2'])) {
 
 defined('_JEXEC') or die;
 /* <pre><?php var_dump($item); ?></pre> */
+
+
+// OK, so generated article stubs don't explicitly record their progenitor, but the URL field does
+// record the generated link to the originating article. Since the URL field isn't used for anything
+// else (and is in fact disabled for display to users, it's _reasonable_ safe to assume that if the
+// `url_a` property is not empty, this article is a stub, so lets get the id of that article so we
+// can then in turn get the data for that article, and use it to set things like headline images
+// for _this_ article if they haven't been locally specified:
+$urls = $item->urls;
+$progenitor_headline_image = [];
+if (!empty($urls)) {
+    $urls = json_decode($urls);
+
+    if (!empty($urls->urla)) {
+        preg_match('#npeu.ox.ac.uk/news/(\d+)-.*#', $urls->urla, $matches);
+
+        if (!empty($matches[1])) {
+            // Pretty certain this is the ID of the progenitor article, so lets get that data:
+            $progenitor_article = JTable::getInstance("content");
+            $progenitor_article->load($matches[1]);
+
+            JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/helpers/fields.php'); //load fields helper
+            $customFieldnames = FieldsHelper::getFields('com_content.article', $matches[1], true); // get custom field names by article id
+            $customFieldIds = array_column($customFieldnames, 'id');
+            $model = JModelLegacy::getInstance('Field', 'FieldsModel', array('ignore_request' => true)); //load fields model
+            $customFieldValues = $model->getFieldValues($customFieldIds, $matches[1]); //Fetch values for custom field Ids
+
+            // If the headline images URL for this article is empty, and the progenitor has one,
+            // use that:
+            if (empty($item->headline_image['headline-image']) && !empty($customFieldValues[1])) {
+                $progenitor_headline_image['headline-image'] = $customFieldValues[1];
+
+                if (!empty($customFieldValues[2])) {
+                    $progenitor_headline_image['headline-image-alt-text'] = $customFieldValues[2];
+                }
+
+                if (!empty($customFieldValues[3])) {
+                    $progenitor_headline_image['headline-image-caption'] = $customFieldValues[3];
+                }
+
+                if (!empty($customFieldValues[4])) {
+                    $progenitor_headline_image['headline-image-credit-line'] = $customFieldValues[4];
+                }
+            }
+        }
+    }
+}
+
 $fields = FieldsHelper::getFields('com_content.article', $item, true);
+$image = false;
+$image_alt = false;
+if (!empty($item->skip_image)) {
+    $image     = $fields[0]->rawvalue;
+    $image_alt = $fields[1]->rawvalue;
+    
+    if (empty($image) && !empty($progenitor_headline_image)) {
+        $image     = $progenitor_headline_image['headline-image'];
+        $image_alt = $progenitor_headline_image['headline-image-alt-text'];
+    }
+}
+
+
+
 
 $i = isset($i) ? $i : 0;
 
